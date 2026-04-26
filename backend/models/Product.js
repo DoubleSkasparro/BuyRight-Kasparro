de84c66c4a7e42bb7c715377d076565d1bc9f545
Sayani Adhikary <155868155+shayoniA@users.noreply.github.com>
2026-04-26 23:09:40 +0530

    Add files via upload

---
+++ backend/models/Product.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  image: String,
+  seller: String,
+  description: String,
+  price: Number,
+  reviews: [
+    {
+      user: String,
+      text: String,
+      rating: Number
+    }
+  ],
+  faqs: [
+    {
+      question: String
+    }
+  ]
+});
+
+module.exports = mongoose.model('Product', productSchema);
---
+++ backend/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, unique: true },
+  email: { type: String, unique: true, sparse: true },
+  password: String,
+  wishlist: [String],
+  cart: [String]
+});
+
+module.exports = mongoose.model('User', userSchema);
